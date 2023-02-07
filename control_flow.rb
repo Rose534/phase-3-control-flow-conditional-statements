b887e388e71b4d30238115a076fbb872d886e375
rose534 <wambuirose534@gmail.com>
2023-02-08 01:40:55 +0300

    lab

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,70 @@
 def admin_login(username, password)
-  # your code here
+  if username == "admin" && password == "12345"
+    return "Access granted"
+
+  elsif username == "ADMIN" && password == "12345"
+    return "Access granted"
+
+  else "Access denied"
 end
+end
+
+admin_login("admin", "12345")
+
+
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    return "It's brisk out there!"
+
+  elsif temperature >= 40 && temperature <= 65
+    return "It\'s a little chilly out there!"
+
+  elsif temperature > 85
+    return "It\'s too dang hot out there!"
+
+  else "It's perfect out there!"
+  end
 end
+
+hows_the_weather(60)
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+     return "Buzz"
+
+  else num 
+  end
 end
 
+fizzbuzz(15)
+
 def calculator(operation, num1, num2)
-  # your code here
+ if operation == "-"
+  return num1 - num2
+
+ elsif operation == "*"
+  return num1 * num2
+
+ elsif operation == "+"
+  return num1 + num2
+
+ elsif operation == "/"
+  return num1 / num2
+
+ elsif operation == "" 
+  return "nil"
+
+ 
 end
+end
+
+calculator("-", 3, 1)
+calculator("*", 3, 2)
+calculator("/", 4, 2)
+
 
